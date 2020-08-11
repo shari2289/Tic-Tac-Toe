1492b0198341e739639b91a5cd4192b4eefa32de
Shari Reyes <shari2289@yahoo.com>
2020-08-10 21:15:54 -0500

    fixed links for js and css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 /*----- constants -----*/
 const lookup = {
-  '1': 'pink',
-  '-1': 'light blue',
-  'null': 'black'
+  "1": "pink",
+  "-1": "teal",
+  null: "white",
 };
 
 const winningCombos = [
@@ -14,63 +13,65 @@ const winningCombos = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
+  [2, 4, 6],
 ];
 
 /*----- app's state (variables) -----*/
-let board;
-let turn;
-let winner;
+let board, turn, winner;
 
 /*----- cached element references -----*/
-const squares = document.querySelectorAll('td div');
-const message = document.querySelector('h1');
+const squares = document.querySelectorAll("td div");
+const message = document.querySelector("h1");
 
 /*----- event listeners -----*/
-document.querySelector('table').addEventListener('click', handleMove);
-document.querySelector('button').addEventListener('click', initialize);
+document.querySelector("table").addEventListener("click", handleMove);
+document.querySelector("button").addEventListener("click", initialize);
 
 /*----- functions -----*/
 
 initialize();
 
 function handleMove(evt) {
-  
-  const idx = parseInt(evt.target.id.replace('sq', ''));
-  
+  const idx = parseInt(evt.target.id.replace("sq", ""));
   if (board[idx] || winner) return;
-  
   board[idx] = turn;
   turn *= -1;
   winner = getWinner();
   render();
-};
+}
 
 function getWinner() {
   for (let i = 0; i < winningCombos.length; i++) {
-    if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
+    if (
+      Math.abs(
+        board[winningCombos[i][0]] +
+          board[winningCombos[i][1]] +
+          board[winningCombos[i][2]]
+      ) === 3
+    )
+      return board[winningCombos[i][0]];
   }
-  
   if (board.includes(null)) return null;
-  return 'T';
-};
-
+  return "T";
+}
 function render() {
-  board.forEach(function(sq, idx) {
+  board.forEach(function (sq, idx) {
     squares[idx].style.background = lookup[sq];
   });
-  if (winner === 'T') {
-    message.innerHTML = 'Its A Tie!';
+  if (winner === "T") {
+    message.innerHTML = "Rats, another tie!";
   } else if (winner) {
-    message.innerHTML = `Congrats ${lookup[winner].toUpperCase()} you won!';} else {
+    message.innerHTML = `Congrats ${lookup[winner].toUpperCase()}!`;
+  } else {
     message.innerHTML = `${lookup[turn].toUpperCase()}'s Turn`;
   }
-};
+}
 
 function initialize() {
   board = [null, null, null, null, null, null, null, null, null];
-
+  // OR initialize like this:
+  // board = new Array(9).fill(null);
   turn = 1;
   winner = null;
   render();
-};
+}
